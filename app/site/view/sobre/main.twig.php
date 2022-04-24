b94af8bf6734d0066dc32bdee9ba8e3f712db02e
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2022-04-24 09:59:37 -0300

    finalizando o curso

--- app/site/view/sobre/main.twig.php
+++ app/site/view/sobre/main.twig.php
@@ -6,13 +6,9 @@
 <h1>Sobre</h1>
 
 <div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam quidem, repudiandae sunt harum in ipsam quos quis aperiam quo consequatur officiis et numquam doloremque accusamus! Exercitationem harum molestias nesciunt.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam quidem, repudiandae sunt harum in ipsam quos quis aperiam quo consequatur officiis et numquam doloremque accusamus! Exercitationem harum molestias nesciunt.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam quidem, repudiandae sunt harum in ipsam quos quis aperiam quo consequatur officiis et numquam doloremque accusamus! Exercitationem harum molestias nesciunt.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam quidem, repudiandae sunt harum in ipsam quos quis aperiam quo consequatur officiis et numquam doloremque accusamus! Exercitationem harum molestias nesciunt.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam quidem, repudiandae sunt harum in ipsam quos quis aperiam quo consequatur officiis et numquam doloremque accusamus! Exercitationem harum molestias nesciunt.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam quidem, repudiandae sunt harum in ipsam quos quis aperiam quo consequatur officiis et numquam doloremque accusamus! Exercitationem harum molestias nesciunt.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam quidem, repudiandae sunt harum in ipsam quos quis aperiam quo consequatur officiis et numquam doloremque accusamus! Exercitationem harum molestias nesciunt.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam quidem, repudiandae sunt harum in ipsam quos quis aperiam quo consequatur officiis et numquam doloremque accusamus! Exercitationem harum molestias nesciunt.</p>
+
+    <p>O Minhas Receitas é o maior site de receitas do Brasil e, desde 2005, reúne uma comunidade de apaixonados por comida. A ideia era criar um site colaborativo, uma espécie de caderno de receitas compartilhado que reunisse os tradicionais biscoitos de família e as novas descobertas na cozinha. Hoje, mais de um milhão de brasileiros passa por aqui todos os dias e nossa missão é ajudar esses leitores a cozinhar com simplicidade e prazer. O blog do Minhas Receitas nasceu em 2016 com o objetivo de expandir nossas receitas em dicas e nos comunicar com a nossa audiência apaixonada pelo mundo da gastronomia. Por aqui você aprende desde o que fazer para um bolo não solar, até como deixar uma pia brilhando com dicas caseiras. Nós também adoramos desvendar curiosidades sobre a culinária de lugares do Brasil ou do mundo e somos experts em fazer seleções de receitas com os ingredientes que você tem em casa. Para nós, o que importa é te inspirar a botar a mão na massa na cozinha! </p>
+    <p>Conheça o time do Minhas Receitas</p>
 </div>
+
 {% endblock %}
