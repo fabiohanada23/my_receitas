a0e4adf71f7606e78ce3bceacfe08f520fe1c4ef
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2022-05-02 22:49:45 -0300

    alteração htaccess e global

--- app/config/global.php
+++ app/config/global.php
@@ -4,6 +4,7 @@ define('BASE', '/my_receitas/');
 define('UNSET_COUNT', 1);
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'myreceitas');
+define('DB_USER', 'id18832490_fabio');
+define('DB_PASS', 'Carrobom0509!');
+define('DB_NAME', 'id18832490_my_receitas');
+
